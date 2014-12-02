7d5957ddec9afc77a94b2b073027a3dfb19b488a
Jonthan Lee <dhmj.bountysource@gmail.com>
2014-12-02 20:40:40 +0800

    Update bookmarklet.js
    Check if it exists, instead of removing node

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,7 +3,7 @@ javascript:(
         if(!window.pmxbot){
             function closeModal(){
                 document.body.style.overflow = "initial";
-                document.getElementById("pmxover").parentNode.removeChild(document.getElementById("pmxover"));
+                o.style.display = "none";
                 window.pmxbot = false;
                 enable_scroll();
             }
@@ -27,34 +27,38 @@ javascript:(
             var base_url = "";
             window.pmxbot = true;
             disable_scroll();
-            var d=document.createElement("div"),
-                o = document.createElement("div");
-            o.setAttribute("style","position:absolute;width:100%;background-color:rgba(0,0,0,.5);z-index:2147483647;text-align:center");
-            o.id="pmxover";
-            o.style.height = window.innerHeight+"px";
-            o.style.top = window.scrollY+"px";
-            o.style.left = window.scrollX+"px";
-            var st = "padding:1em;background:#fff;border:1px #ccc solid;margin:.5em;width:90%;border-radius:0;",
-                t="font-family:Helvetica,Arial";
-            d.setAttribute("style","background-color:#eee;padding: 1em;text-align:center;width:25em;position:relative;top:50%;transform:translateY(-50%);margin:auto;box-shadow:0 0 30px #555;border-radius:0.25em;"+t);
-            document.body.appendChild(o);
-            o.appendChild(d);
-            document.body.style.overflow = "hidden";
-            var f=document.createElement("form"),
-                s=document.createElement("input"),
-                c=document.createElement("input");
-            s.setAttribute("type","submit");
-            c.setAttribute("type","button");
-            f.innerHTML = "<span style='font-size:2em;float:left;padding-left:.2em;color:#666'>pmxbot</span><input id='pmxchn' placeholder='Channel' style='"+st+t+"'><br><input id='pmxmsg' placeholder='Message' style='"+st+t+"'>";
-            var n= "margin:.5em;width:42.5%;cursor:pointer;padding:.5em;";
-            s.setAttribute("style","background:#5dc251;border:0;box-shadow:0 3px 0 #409936;color:#dbf0df;"+n+t);
-            c.setAttribute("style","background:#f66;border:0;box-shadow:0 3px 0 #e01414;color:#fcc;"+n+t);
-            s.setAttribute("value", "Submit");
-            c.setAttribute("value", "Cancel");
-            f.setAttribute("style", "margin-bottom:0");
-            f.appendChild(s);
-            f.appendChild(c);
-            d.appendChild(f);
+            if (document.getElementById("pmxover"))
+                document.getElementById("pmxover").style.display = "";
+            else {
+                var d=document.createElement("div"),
+                    o = document.createElement("div");
+                o.setAttribute("style","position:absolute;width:100%;background-color:rgba(0,0,0,.5);z-index:2147483647;text-align:center");
+                o.id="pmxover";
+                o.style.height = window.innerHeight+"px";
+                o.style.top = window.scrollY+"px";
+                o.style.left = window.scrollX+"px";
+                var st = "padding:1em;background:#fff;border:1px #ccc solid;margin:.5em;width:90%;border-radius:0;",
+                    t="font-family:Helvetica,Arial";
+                d.setAttribute("style","background-color:#eee;padding: 1em;text-align:center;width:25em;position:relative;top:50%;transform:translateY(-50%);margin:auto;box-shadow:0 0 30px #555;border-radius:0.25em;"+t);
+                document.body.appendChild(o);
+                o.appendChild(d);
+                document.body.style.overflow = "hidden";
+                var f=document.createElement("form"),
+                    s=document.createElement("input"),
+                    c=document.createElement("input");
+                s.setAttribute("type","submit");
+                c.setAttribute("type","button");
+                f.innerHTML = "<span style='font-size:2em;float:left;padding-left:.2em;color:#666'>pmxbot</span><input id='pmxchn' placeholder='Channel' style='"+st+t+"'><br><input id='pmxmsg' placeholder='Message' style='"+st+t+"'>";
+                var n= "margin:.5em;width:42.5%;cursor:pointer;padding:.5em;";
+                s.setAttribute("style","background:#5dc251;border:0;box-shadow:0 3px 0 #409936;color:#dbf0df;"+n+t);
+                c.setAttribute("style","background:#f66;border:0;box-shadow:0 3px 0 #e01414;color:#fcc;"+n+t);
+                s.setAttribute("value", "Submit");
+                c.setAttribute("value", "Cancel");
+                f.setAttribute("style", "margin-bottom:0");
+                f.appendChild(s);
+                f.appendChild(c);
+                d.appendChild(f);
+            }
             document.getElementById("pmxmsg").focus();
             document.getElementById("pmxchn").value=localStorage.getItem("strchn")||"";
             document.getElementById("pmxchn").addEventListener("keyup", function(){
